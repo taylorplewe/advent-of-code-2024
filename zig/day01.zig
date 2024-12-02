aee6f168bfecc6a943c247e8dfd955c0f4cee7ff
Taylor Plewe <tplewe@outlook.com>
2024-12-02 03:53:37 +0000

    zig day 1 part 2, clean up

--- zig/day01.zig
+++ zig/day01.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 
+const isPartTwo = true;
+
 pub fn main() !void {
     // init
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     var lefts = std.ArrayList(u32).init(arena.allocator());
     var rights = std.ArrayList(u32).init(arena.allocator());
+    var rightNumOccurrences = std.AutoHashMap(u32, u32).init(arena.allocator());
 
     // get iterator over each line
     const args = try std.process.argsAlloc(arena.allocator());
@@ -28,19 +31,29 @@ pub fn main() !void {
         const right = try std.fmt.parseInt(u32, right_str, 10);
         try lefts.append(left);
         try rights.append(right);
+        if (rightNumOccurrences.get(right) == null) {
+            try rightNumOccurrences.put(right, 0);
+        }
+        try rightNumOccurrences.put(right, rightNumOccurrences.get(right).? + 1);
     }
 
-    // sort lists
-    std.sort.heap(u32, lefts.items, {}, std.sort.asc(u32));
-    std.sort.heap(u32, rights.items, {}, std.sort.asc(u32));
+    if (!isPartTwo) {
+        // sort lists
+        std.sort.heap(u32, lefts.items, {}, std.sort.asc(u32));
+        std.sort.heap(u32, rights.items, {}, std.sort.asc(u32));
 
-    // add up differences
-    var sum: u64 = 0;
-    for (lefts.items, rights.items) |l, r| {
-        if (l > r)
-            sum += l - r
-        else
-            sum += r - l;
+        // add up differences
+        var sum: u64 = 0;
+        for (lefts.items, rights.items) |l, r| {
+            sum += if (l > r) (l - r) else (r - l);
+        }
+        std.debug.print("sum of diffs: {d}\n", .{sum});
+    } else {
+        var sum: u64 = 0;
+        for (lefts.items) |val| {
+            if (rightNumOccurrences.get(val)) |numOccurrences|
+                sum += val * numOccurrences;
+        }
+        std.debug.print("sum of all pt. 2 vals: {d}\n", .{sum});
     }
-    std.debug.print("sum of diffs: {d}\n", .{sum});
 }
